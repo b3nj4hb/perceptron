f6939f177c338a78f50409d6ff2816b8662f691a
b3nj4hb <benjalive12@gmail.com>
2024-07-04 19:57:22 -0500

    feat: Add basic admin view
    Initial setup of a basic admin view with a placeholder "Hello world!" message. This will be built upon in future commits.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AdminIndexComponent } from './admin/pages/admin-index/admin-index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',title: 'Vista Admin',component: AdminIndexComponent}
+];
