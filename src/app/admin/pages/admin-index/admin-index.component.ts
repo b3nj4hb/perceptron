a73c93a16594cc1349a7fcf7953388d4c58e63db
b3nj4hb <benjalive12@gmail.com>
2024-07-04 20:32:49 -0500

    feat: Introduce reusable title section component
    Adds a reusable component for displaying section titles, improving code consistency and maintainability. The `AdminIndex` component now uses this component to display its title.

--- src/app/admin/pages/admin-index/admin-index.component.ts
+++ src/app/admin/pages/admin-index/admin-index.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TitleSectionComponent } from '../../components/title-section/title-section.component';
 
 @Component({
   selector: 'app-admin-index',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleSectionComponent
+  ],
   templateUrl: './admin-index.component.html',
   styleUrl: './admin-index.component.scss'
 })
