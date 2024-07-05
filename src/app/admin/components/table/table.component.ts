37029f324acd1b0c1930c5c9cc27c50490f9c991
David Rey <neisserrey@upeu.edu.pe>
2024-07-05 15:16:09 -0500

    feat: :boom: Feature 2: Apply filters to students array.

--- src/app/admin/components/table/table.component.ts
+++ src/app/admin/components/table/table.component.ts
@@ -2,40 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../../services/student.service';
 import { CommonModule } from '@angular/common';
 import { Chart } from 'chart.js/auto';
+import { Student } from '../../classes/student';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent implements OnInit {
-  students: any;
-  constructor(private StudentService: StudentService) { }
+  students: Student[] = [];
+
+  filteredStudents: Student[] = [];
+  filter = '';
+  filterSection = '';
+  filterGrade = '';
+  sections: string[] = [];
+  grades: number[] = [];
+
+  constructor(private StudentService: StudentService) {}
   public chart: Chart;
 
   ngOnInit(): void {
-    this.listStudent()
-    this.showChart()
+    this.listStudent();
+    this.showChart();
   }
   listStudent() {
-    this.StudentService.getStudent().subscribe(data => {
+    this.StudentService.getStudent().subscribe((data) => {
       this.students = data;
       // console.log(data)
-      console.log(this.students[0])
+      console.log(this.students[0]);
+      this.applyFilters();
+      this.buildGradeAndSectionArray();
       // console.log(this.students[0]['id'])
-    })
+    });
   }
   showChart() {
     const data = {
-      datasets: [{
-        data: [2, 4],
-        label: 'Compentencias'
-      }],
-      labels: ['Math', 'Science']
-    }
-    this.chart = new Chart("chart", {
+      datasets: [
+        {
+          data: [2, 4],
+          label: 'Compentencias',
+        },
+      ],
+      labels: ['Math', 'Science'],
+    };
+    this.chart = new Chart('chart', {
       data,
       type: 'bar',
       options: {
@@ -44,10 +58,35 @@ export class TableComponent implements OnInit {
           x: {
             min: 1,
             max: 5,
-          }
-        }
-      }
-    })
+          },
+        },
+      },
+    });
   }
 
+  applyFilters(): void {
+    this.filteredStudents = this.students.filter((student) => {
+      return (
+        (!this.filter ||
+          student.name.toLowerCase().includes(this.filter.toLowerCase()) ||
+          student.lastName.toLowerCase().includes(this.filter.toLowerCase()) ||
+          student.code.toString().includes(this.filter.toLowerCase())) &&
+        (!this.filterSection || student.section === this.filterSection) &&
+        (!this.filterGrade || student.grade === parseInt(this.filterGrade))
+      );
+    });
+  }
+
+  buildGradeAndSectionArray() {
+    const sectionsSet = new Set<string>();
+    const gradesSet = new Set<number>();
+
+    this.students.forEach((student) => {
+      sectionsSet.add(student.section);
+      gradesSet.add(student.grade);
+    });
+    this.sections = Array.from(sectionsSet).sort(); // Ordenar secciones alfabéticamente
+    this.grades = Array.from(gradesSet).sort((a, b) => a - b); // Ordenar grados numéricamente
+    console.log(this.grades);
+  }
 }
--- src/app/admin/services/student.service.ts
+++ src/app/admin/services/student.service.ts
@@ -2,20 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Student } from '../classes/student';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentService {
-
   HttpOptions = {
-    Headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    Headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  private urlEndPoint: string = 'https://66874e2b0bc7155dc0174111.mockapi.io/api/listStudents/student'
+  private urlEndPoint: string =
+    'https://66874e2b0bc7155dc0174111.mockapi.io/api/listStudents/student';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
   getStudent() {
-    return this.http.get(this.urlEndPoint);
+    return this.http.get<Student[]>(this.urlEndPoint);
   }
 }
