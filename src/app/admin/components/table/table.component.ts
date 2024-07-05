567bd571befb429237cd0e8adc7833a457e6c784
b3nj4hb <benjalive12@gmail.com>
2024-07-05 10:47:59 -0500

    feat: Introduce chart.js for performance visualization
    Adds a basic chart using Chart.js to visualize student performance. This chart will display the scores for Math and Science, providing a visual representation of individual student strengths and weaknesses.

--- src/app/admin/components/table/table.component.ts
+++ src/app/admin/components/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../../services/student.service';
 import { CommonModule } from '@angular/common';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-table',
@@ -12,8 +13,11 @@ import { CommonModule } from '@angular/common';
 export class TableComponent implements OnInit {
   students: any;
   constructor(private StudentService: StudentService) { }
+  public chart: Chart;
+
   ngOnInit(): void {
     this.listStudent()
+    this.showChart()
   }
   listStudent() {
     this.StudentService.getStudent().subscribe(data => {
@@ -23,4 +27,27 @@ export class TableComponent implements OnInit {
       // console.log(this.students[0]['id'])
     })
   }
+  showChart() {
+    const data = {
+      datasets: [{
+        data: [2, 4],
+        label: 'Compentencias'
+      }],
+      labels: ['Math', 'Science']
+    }
+    this.chart = new Chart("chart", {
+      data,
+      type: 'bar',
+      options: {
+        indexAxis: 'y',
+        scales: {
+          x: {
+            min: 1,
+            max: 5,
+          }
+        }
+      }
+    })
+  }
+
 }
