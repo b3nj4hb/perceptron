39c731c0ec1b84329900e6facd9968147733faee
b3nj4hb <benjalive12@gmail.com>
2024-07-04 21:12:32 -0500

    feat: Add page layout and action button to admin index
    Adds the `PageLayoutComponent` and `ActionButtonComponent` to the `admin-index` page, providing a more structured and functional layout for the admin section. This improves the overall UI and provides a better user experience for managing the admin area.

--- src/app/admin/pages/admin-index/admin-index.component.ts
+++ src/app/admin/pages/admin-index/admin-index.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { TitleSectionComponent } from '../../components/title-section/title-section.component';
+import { PageLayoutComponent } from '../../layouts/page-layout/page-layout.component';
+import { ActionButtonComponent } from '../../components/action-button/action-button.component';
 
 @Component({
   selector: 'app-admin-index',
   standalone: true,
   imports: [
-    TitleSectionComponent
+    TitleSectionComponent,
+    PageLayoutComponent,
+    ActionButtonComponent
   ],
   templateUrl: './admin-index.component.html',
   styleUrl: './admin-index.component.scss'
